6a3804c72a8868b46d77091e39b3fa120927052c
flatoutta <58313982+flatoutta@users.noreply.github.com>
2020-01-18 14:58:39 +0900

    Add files via upload

---
+++ edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charaset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>MyProfile</title>
+  </head>
+  <body>
+    <h1>MyProfileプロフィール編集画面</h1>
+  </body>
+</html>
